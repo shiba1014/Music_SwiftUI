e70cd08887452812a59aa463893d4774f47a3199
shiba1014 <shiba.png@gmail.com>
2020-07-08 14:24:15 +0900

    Initial Commit

---
+++ Music/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Music
+//
+//  Created by Satsuki Hashiba on 2020/07/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Music/MusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicApp.swift
+//  Music
+//
+//  Created by Satsuki Hashiba on 2020/07/08.
+//
+
+import SwiftUI
+
+@main
+struct MusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
